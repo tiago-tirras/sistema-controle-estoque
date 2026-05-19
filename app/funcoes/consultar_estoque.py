52dacd22c16f5cdae1c4246f49f493045f19b9c5
Tiago Silva <tiagoandrade1682@gmail.com>
2026-05-19 20:06:49 -0300

    Adiconado função consultar estoque

---
+++ app/funcoes/consultar_estoque.py
@@ -0,0 +1,16 @@
+import app.dados as dados 
+
+def consultar_estoque():
+    for produto in estoque:
+        print(f"{produto['id']} - {produto['nome']}")
+    
+    try:
+        produto_id = int(input('Digite o ID do produto: '))
+    except ValueError:
+        print('Apenas números inteiros são válidos!')
+    
+    for produto in dados.estoque:
+        if produto['id'] == produto_id:
+            print(f"{produto['nome']} | Quantidade disponivel: {produto['quantidade']}")
+            return
+    print('Produto não encontrado no estoque!')
