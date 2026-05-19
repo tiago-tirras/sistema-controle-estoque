db5f5202237e987d5fbeb32fae3508923394cb41
Tiago Silva <tiagoandrade1682@gmail.com>
2026-05-19 19:25:13 -0300

    Adicionado funções Registrar entrada e saida

---
+++ app/funcoes/registrar_saida.py
@@ -0,0 +1,21 @@
+import app.dados as dados
+
+def registrar_saida():
+    for produto in dados.estoque:
+        print(f"{produto['id']} - {produto['nome']} | Quantidade: {produto['quantidade']}")
+    
+    try:
+        produto_id = int(input('Digite o ID do produto: '))
+        nova_quantidade = int(input('Digite a quantidade que será adicionada ao produto: '))
+    except ValueError:
+        print('Apenas números inteiros são válidos!')
+
+    for produto in dados.estoque:
+        if produto['id'] == produto_id:
+            if produto['quantidade'] >= nova_quantidade:
+                produto['quantidade'] -= nova_quantidade
+                print(f"Saida registrada com sucesso! Quantidade atualizada do produto: {produto['quantidade']}")
+            else:
+                print('Quantidade insuficiente no estoque!')
+                return
+    print('Produto não encontrado no estoque!')            
