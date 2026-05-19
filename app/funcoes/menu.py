0c666ca76e1ffe3dc1b6d9bc3933299f874700a5
Tiago Silva <tiagoandrade1682@gmail.com>
2026-05-19 20:33:35 -0300

    Adicionado função para exibir menu

---
+++ app/funcoes/menu.py
@@ -0,0 +1,9 @@
+def menu():
+        print('\n---MENU DE OPÇÕES---')
+        print('1. cadastrar produto ao estoque')
+        print('2. registrar entrada')
+        print('3. registrar saida')
+        print('4. consultar estoque')
+        print('5. alertar estoque baixo')
+        print('7. mostrar estoque completo')
+        print('8. sair ')
